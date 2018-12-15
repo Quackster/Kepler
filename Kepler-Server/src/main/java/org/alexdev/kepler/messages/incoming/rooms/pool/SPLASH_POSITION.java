c58cc8ab8d385afe6cc00f71be5ec1c25307960a
Alex <quacksterr.15@gmail.com>
2018-12-15 21:44:13 +1000

    Don't allow walking until they're at the end of the ladder

--- Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/rooms/pool/SPLASH_POSITION.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/rooms/pool/SPLASH_POSITION.java
@@ -1,18 +1,17 @@
 package org.alexdev.kepler.messages.incoming.rooms.pool;
 
-import org.alexdev.kepler.game.room.enums.StatusType;
+import org.alexdev.kepler.game.GameScheduler;
 import org.alexdev.kepler.game.item.Item;
 import org.alexdev.kepler.game.pathfinder.Position;
 import org.alexdev.kepler.game.player.Player;
 import org.alexdev.kepler.game.room.Room;
-import org.alexdev.kepler.game.room.mapping.RoomTile;
+import org.alexdev.kepler.game.room.enums.StatusType;
 import org.alexdev.kepler.messages.outgoing.rooms.items.SHOWPROGRAM;
-import org.alexdev.kepler.messages.outgoing.rooms.user.USER_STATUSES;
 import org.alexdev.kepler.messages.types.MessageEvent;
 import org.alexdev.kepler.server.netty.streams.NettyRequest;
 import org.alexdev.kepler.util.StringUtil;
 
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class SPLASH_POSITION implements MessageEvent {
     @Override
@@ -45,37 +44,43 @@ public class SPLASH_POSITION implements MessageEvent {
 
         room.send(new SHOWPROGRAM(new String[] { "BIGSPLASH", "POSITION", contents,}));
 
-        player.getRoomUser().setWalkingAllowed(true);
         player.getRoomUser().setDiving(false);
-        player.getRoomUser().walkTo(20, 19);
+        player.getRoomUser().walkTo(18, 19);
+
+        // Allow walkng after 3 seconds
+        GameScheduler.getInstance().getService().schedule(() -> {
+            player.getRoomUser().setWalkingAllowed(true);
+        }, 3, TimeUnit.SECONDS);
 
         currentItem.showProgram("open");
 
-        int total = 0;
-        int sum = 0;
-        double finalScore = 0;
+        GameScheduler.getInstance().getService().schedule(() -> {
+            int total = 0;
+            int sum = 0;
+            double finalScore = 0;
 
-        for (Player p : room.getEntityManager().getPlayers()) {
-            if (p.getDetails().getId() == player.getDetails().getId()) {
-                continue;
+            for (Player p : room.getEntityManager().getPlayers()) {
+                if (p.getDetails().getId() == player.getDetails().getId()) {
+                    continue;
+                }
+
+                if (p.getRoomUser().getLidoVote() > 0) {
+                    sum += p.getRoomUser().getLidoVote();
+                    total++;
+                }
             }
 
-            if (p.getRoomUser().getLidoVote() > 0) {
-                sum += p.getRoomUser().getLidoVote();
-                total++;
+            room.send(new SHOWPROGRAM(new String[]{"cam1", "targetcamera", String.valueOf(player.getRoomUser().getInstanceId())}));
+
+            if (total > 0) {
+                finalScore = StringUtil.format((double) sum / total);
             }
-        }
 
-        room.send(new SHOWPROGRAM(new String[]{"cam1", "targetcamera", String.valueOf(player.getRoomUser().getInstanceId())}));
+            room.send(new SHOWPROGRAM(new String[]{"cam1", "showtext", (player.getDetails().getName() + "'s\n score: " + finalScore)}));
 
-        if (total > 0) {
-            finalScore = StringUtil.format((double) sum / total);
-        }
-
-        room.send(new SHOWPROGRAM(new String[]{"cam1", "showtext", (player.getDetails().getName() + "'s\n score: " + finalScore)}));
-
-        for (Player p : room.getEntityManager().getPlayers()) {
-            p.getRoomUser().setLidoVote(0);
-        }
+            for (Player p : room.getEntityManager().getPlayers()) {
+                p.getRoomUser().setLidoVote(0);
+            }
+        }, 1, TimeUnit.SECONDS);
     }
 }
