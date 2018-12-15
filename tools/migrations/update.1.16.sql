3276db380d8f932feb6984be1309c1cade5ff176
Alex <quacksterr.15@gmail.com>
2018-12-15 20:37:15 +1000

    Update update.1.16.sql

--- tools/migrations/update.1.16.sql
+++ tools/migrations/update.1.16.sql
@@ -14,3 +14,5 @@ ALTER TABLE `users` ADD `club_gift_due` BIGINT(11) NOT NULL DEFAULT '0' AFTER `c
 UPDATE catalogue_items SET sale_code = (SELECT sprite FROM items_definitions WHERE items_definitions.id = catalogue_items.definition_id) WHERE page_id = 27 AND is_package = 0;
 
 ALTER TABLE `settings` CHANGE `value` `value` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '';
+
+UPDATE users SET club_subscribed = 0;
