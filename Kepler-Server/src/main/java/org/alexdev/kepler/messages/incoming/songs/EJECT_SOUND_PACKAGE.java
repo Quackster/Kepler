0083d850a7dbc8b638f9d002811349695929ba62
Alex <quacksterr.15@gmail.com>
2019-01-22 07:53:26 +1000

    Don't recreate soundset item when ejecting

--- Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/songs/EJECT_SOUND_PACKAGE.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/songs/EJECT_SOUND_PACKAGE.java
@@ -1,15 +1,13 @@
 package org.alexdev.kepler.messages.incoming.songs;
 
-import org.alexdev.kepler.dao.mysql.ItemDao;
 import org.alexdev.kepler.dao.mysql.SongMachineDao;
 import org.alexdev.kepler.game.item.Item;
-import org.alexdev.kepler.game.item.ItemManager;
-import org.alexdev.kepler.game.item.base.ItemDefinition;
+import org.alexdev.kepler.game.item.base.ItemBehaviour;
 import org.alexdev.kepler.game.moderation.Fuseright;
 import org.alexdev.kepler.game.player.Player;
 import org.alexdev.kepler.game.room.Room;
-import org.alexdev.kepler.messages.outgoing.songs.USER_SOUND_PACKAGES;
 import org.alexdev.kepler.messages.outgoing.songs.SOUND_PACKAGES;
+import org.alexdev.kepler.messages.outgoing.songs.USER_SOUND_PACKAGES;
 import org.alexdev.kepler.messages.types.MessageEvent;
 import org.alexdev.kepler.server.netty.streams.NettyRequest;
 
@@ -38,26 +36,41 @@ public class EJECT_SOUND_PACKAGE implements MessageEvent {
         int slotId = reader.readInt();
         Map<Integer, Integer> tracks = SongMachineDao.getTracks(room.getItemManager().getSoundMachine().getId());
 
-        if (tracks.containsKey(slotId)) {
-            int songSoundId = tracks.get(slotId);
-
-            ItemDefinition track = ItemManager.getInstance().getDefinitionBySprite("sound_set_" + songSoundId);
-
-            Item item = new Item();
-            item.setOwnerId(player.getDetails().getId());
-            item.setDefinitionId(track.getId());
-
-            ItemDao.newItem(item);
-
-            player.getInventory().addItem(item);
-            player.getInventory().getView("new");
+        if (!tracks.containsKey(slotId)) {
+            return;
         }
 
         SongMachineDao.removeTrack(room.getItemManager().getSoundMachine().getId(), slotId);
 
+        int songSoundId = tracks.get(slotId);
+        Item soundset = null;
+
+        for (Item item : player.getInventory().getItems()) {
+            if (!item.isHidden()) {
+                continue;
+            }
+
+            if (!item.hasBehaviour(ItemBehaviour.SOUND_MACHINE_SAMPLE_SET)) {
+                continue;
+            }
+
+            if (Integer.parseInt(item.getDefinition().getSprite().replace("sound_set_", "")) == songSoundId) {
+                soundset = item;
+                break;
+            }
+        }
+
+        if (soundset == null) {
+            return;
+        }
+
+
+        soundset.setHidden(false);
+        soundset.save();
+
+        player.getInventory().getView("new");
+
         player.send(new SOUND_PACKAGES(SongMachineDao.getTracks(room.getItemManager().getSoundMachine().getId())));
         player.send(new USER_SOUND_PACKAGES(player.getInventory().getSoundsets()));
-
-        // TODO: resend song with removed sounds from soundset (check first if client maybe requests from other packet)
     }
 }
