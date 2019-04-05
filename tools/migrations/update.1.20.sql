8c23b5afc2e205751fb98065bde1cb085743c4b7
Alex <quacksterr.15@gmail.com>
2019-04-05 13:18:46 +1000

    Create update.1.20.sql

---
+++ tools/migrations/update.1.20.sql
@@ -0,0 +1,24 @@
+CREATE TABLE `room_chatlogs` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `timestamp` bigint(20) NOT NULL,
+  `chat_type` tinyint(1) NOT NULL,
+  `message` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `room_chatlogs`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `room_chatlogs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  
+ CREATE TABLE `users_bans` (
+  `ban_type` enum('MACHINE_ID','IP_ADDRESS','USER_ID') NOT NULL,
+  `banned_value` varchar(250) NOT NULL,
+  `banned_message` text NOT NULL DEFAULT '',
+  `banned_until` bigint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `users_bans`
+  ADD PRIMARY KEY (`banned_value`); 
