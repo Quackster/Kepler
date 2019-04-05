bcf4da0f33536233c9f945b7a152c05660016257
Alex <quacksterr.15@gmail.com>
2019-04-05 13:19:17 +1000

    Update update.1.20.sql

--- tools/migrations/update.1.20.sql
+++ tools/migrations/update.1.20.sql
@@ -22,3 +22,16 @@ ALTER TABLE `room_chatlogs`
 
 ALTER TABLE `users_bans`
   ADD PRIMARY KEY (`banned_value`); 
+  
+CREATE TABLE `rooms_events` (
+  `room_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `expire_time` bigint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `rooms_events`
+  ADD PRIMARY KEY (`room_id`),
+  ADD UNIQUE KEY `room_id` (`room_id`);
