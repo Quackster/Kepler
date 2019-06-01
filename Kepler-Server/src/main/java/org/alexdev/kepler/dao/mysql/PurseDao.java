d5e344cffc328f51c8e34528d33de607fb606938
DESKTOP-QJ9P3L2\Heath Sargent <killerloader@hotmail.com>
2019-06-02 01:09:02 +1000

    Added voucher SQL class which redeems a voucher Updated the current REDEEM_VOUCHER incomming message to properly redeem the voucher

---
+++ Kepler-Server/src/main/java/org/alexdev/kepler/dao/mysql/PurseDao.java
@@ -0,0 +1,51 @@
+package org.alexdev.kepler.dao.mysql;
+
+import org.alexdev.kepler.dao.Storage;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class PurseDao {
+
+    /**
+     * Redeems a voucher.
+	 * Gets the voucher and then deletes it from the voucher table.
+     *
+     * @param voucherCode the string voucher code to redeem
+     * @return the amount of credits redeemed or -1 if no voucher was found.
+     */
+    public static int redeemVoucher(String voucherCode) {
+
+        Connection sqlConnection = null;
+        PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		
+        try {
+			//Get the voucher
+            sqlConnection = Storage.getStorage().getConnection();
+            preparedStatement = Storage.getStorage().prepare("SELECT Credits FROM vouchers WHERE VoucherCode = ?", sqlConnection);
+            preparedStatement.setString(1, voucherCode);
+            resultSet = preparedStatement.executeQuery();
+            
+            if (resultSet.next()) {
+				//Delete the voucher
+				preparedStatement = Storage.getStorage().prepare("DELETE FROM vouchers WHERE VoucherCode = ?", sqlConnection);
+				preparedStatement.setString(1, voucherCode);
+				preparedStatement.executeQuery();
+
+                return resultSet.getInt("Credits");
+            }
+            
+        } catch (Exception e) {
+            Storage.logError(e);
+        } finally {
+            Storage.closeSilently(resultSet);
+            Storage.closeSilently(preparedStatement);
+            Storage.closeSilently(sqlConnection);
+        }
+
+		//No voucher found.
+        return -1;    
+    }
+}
--- Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/purse/REDEEM_VOUCHER.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/purse/REDEEM_VOUCHER.java
@@ -1,18 +1,36 @@
 package org.alexdev.kepler.messages.incoming.purse;
 
 import org.alexdev.kepler.game.player.Player;
+import org.alexdev.kepler.dao.mysql.PurseDao;
 import org.alexdev.kepler.messages.outgoing.purse.VOUCHER_REDEEM_ERROR;
 import org.alexdev.kepler.messages.outgoing.purse.VOUCHER_REDEEM_OK;
 import org.alexdev.kepler.messages.outgoing.purse.VOUCHER_REDEEM_ERROR.RedeemError;
 import org.alexdev.kepler.messages.types.MessageEvent;
 import org.alexdev.kepler.server.netty.streams.NettyRequest;
 
-public class REDEEM_VOUCHER implements MessageEvent {
+import org.alexdev.kepler.dao.mysql.*;
+import org.alexdev.kepler.messages.outgoing.user.currencies.CREDIT_BALANCE;
 
+public class REDEEM_VOUCHER implements MessageEvent {
+	
     @Override
     public void handle(Player player, NettyRequest reader) {
         String voucher = reader.readString();
-        //player.send(new VOUCHER_REDEEM_OK());
-        player.send(new VOUCHER_REDEEM_ERROR(RedeemError.PRODUCT_DELIVERY_FAILED));
+		
+		//Check and get voucher
+		int voucherCredits = PurseDao.redeemVoucher(voucher);
+		
+		if(voucherCredits == -1)
+		{
+			//No voucher was found
+			player.send(new VOUCHER_REDEEM_ERROR(RedeemError.PRODUCT_DELIVERY_FAILED));
+		}
+		else
+		{
+			//A voucher was found with more than 0 credits
+			player.send(new VOUCHER_REDEEM_OK());
+			CurrencyDao.increaseCredits(player.getDetails(), voucherCredits);
+			player.send(new CREDIT_BALANCE(player.getDetails()));
+		}
     }
 }
