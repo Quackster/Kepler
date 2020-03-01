ff8b76cdf69db985bb12afdcce329022882d624a
Quackster <quacksterr.15@gmail.com>
2020-03-01 15:19:54 +1000

    Start commit of v1.22.5 changes

---
+++ tools/migrations/update.1.22.5.sql
@@ -0,0 +1,3 @@
+UPDATE `items_definitions` SET `behaviour`='solid,requires_touching_for_interaction,custom_data_true_false,teleporter,redirect_rotation_2' WHERE  `id`=100;
+UPDATE `items_definitions` SET `behaviour`='solid,requires_touching_for_interaction,custom_data_true_false,teleporter,redirect_rotation_2' WHERE  `id`=101;
+UPDATE `items_definitions` SET `behaviour`='solid,requires_touching_for_interaction,custom_data_true_false,teleporter,redirect_rotation_2' WHERE  `id`=102;
