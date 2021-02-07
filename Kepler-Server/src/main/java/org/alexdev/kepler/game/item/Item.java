afa487750955e92de28bb6ff20598c19697a7055
Patrick Nærvig Wøhlk <patrick@webbanditten.dk>
2021-02-07 12:50:49 +0100

    Check if rotating into other solid objects.

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/item/Item.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/item/Item.java
@@ -377,6 +377,12 @@ public class Item {
             if (highestItem != null && highestItem.getId() != item.getId()) {
                 if (!this.canPlaceOnTop(item, highestItem)) {
                     return false;
+                } else {
+                    if(highestItem.getPosition().getZ() == item.getPosition().getZ() && isRotation) {
+                        if(highestItem.getDefinition().hasBehaviour(ItemBehaviour.SOLID)) {
+                           return false;
+                        }
+                    }
                 }
             }
 
