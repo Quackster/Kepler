fa52f91ab025c9f689907e5e5fc003248d9319f7
Alex <quacksterr.15@gmail.com>
2021-12-14 13:53:51 +1000

    Fix getting stuck in pool booth

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/item/interactors/types/PoolInteractor.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/item/interactors/types/PoolInteractor.java
@@ -4,6 +4,7 @@ import org.alexdev.kepler.game.entity.Entity;
 import org.alexdev.kepler.game.entity.EntityType;
 import org.alexdev.kepler.game.item.Item;
 import org.alexdev.kepler.game.pathfinder.Position;
+import org.alexdev.kepler.game.player.Player;
 import org.alexdev.kepler.game.room.Room;
 import org.alexdev.kepler.game.room.enums.StatusType;
 import org.alexdev.kepler.game.room.mapping.RoomTile;
@@ -26,7 +27,7 @@ public class PoolInteractor {
             if (fromItem != null) {
                 // Check if they have swimmers before trying to enter pool
                 if (fromItem.getDefinition().getSprite().equals("poolEnter") ||
-                        fromItem.getDefinition().getSprite().equals("poolExit")) {
+                    fromItem.getDefinition().getSprite().equals("poolExit")) {
                     return entity.getDetails().getPoolFigure().length() > 0;
                 }
             }
@@ -34,40 +35,26 @@ public class PoolInteractor {
             if (toItem != null) {
                 // Check if they have swimmers before trying to enter pool
                 if (toItem.getDefinition().getSprite().equals("poolEnter") ||
-                        toItem.getDefinition().getSprite().equals("poolExit")) {
+                    toItem.getDefinition().getSprite().equals("poolExit")) {
                     return entity.getDetails().getPoolFigure().length() > 0;
                 }
                 // Don't allow to "enter" the pool if they're already swimming
                 if (entity.getRoomUser().containsStatus(StatusType.SWIM) &&
-                        toItem.getDefinition().getSprite().equals("poolEnter")) {
+                    toItem.getDefinition().getSprite().equals("poolEnter")) {
                     return false;
                 }
 
                 // Don't allow to "leave" the pool if they're not swimming
                 if (!entity.getRoomUser().containsStatus(StatusType.SWIM) &&
-                        toItem.getDefinition().getSprite().equals("poolExit")) {
+                    toItem.getDefinition().getSprite().equals("poolExit")) {
                     return false;
                 }
-
-                /*
-                // Don't allow users to cut people in queue, force them to garound
-                if (toItem.getDefinition().getSprite().contains("queue_tile2")) {
-                    if (entity.getType() == EntityType.PLAYER && ((Player)entity).getNetwork().isFlashConnection()) {
-                        return false;
-                    }
-                }
-                */
-
+                
                 // Don't allow people to enter the booth if it's closed, or don't allow
                 // if they attempt to use the pool lift without swimmers
                 if (toItem.getDefinition().getSprite().equals("poolBooth") ||
-                        toItem.getDefinition().getSprite().equals("poolLift")) {
-
-                    if (toItem.getCurrentProgramValue().equals("close")) {
-                        return false;
-                    } else {
-                        return entity.getDetails().getPoolFigure().length() > 0;
-                    }
+                    toItem.getDefinition().getSprite().equals("poolLift")) {
+                    return !toItem.getCurrentProgramValue().equals("close");
                 }
             }
         }
