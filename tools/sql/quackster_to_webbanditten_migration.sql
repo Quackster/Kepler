3227fa38f6ac8fe39878829b3fe47ced24ac6ae6
Patrick Nærvig Wøhlk <patrick@webbanditten.dk>
2022-03-12 14:53:33 +0100

    Update quackster_to_webbanditten_migration.sql

--- tools/sql/quackster_to_webbanditten_migration.sql
+++ tools/sql/quackster_to_webbanditten_migration.sql
@@ -1,34 +1,46 @@
+CREATE TABLE `command_queue` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`executed` TINYINT(1) NOT NULL DEFAULT 0,
+	`command` VARCHAR(255) NOT NULL,
+	`arguments` VARCHAR(1024) NOT NULL,
+	`time` TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `users_bans`;
+CREATE TABLE `users_bans` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ban_type` enum('MACHINE_ID','IP_ADDRESS','USER_ID') NOT NULL,
+  `message` text NOT NULL,
+  `banned_until` bigint(11) NOT NULL,
+  `user_id` int(11) NOT NULL DEFAULT 0,
+  `ip` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
 ALTER TABLE `users`
 ADD `status` varchar(50) NOT NULL DEFAULT 'offline' AFTER `snowstorm_points`,
 ADD  `group_id` int(11) NOT NULL DEFAULT 0  AFTER `status`;
 
 
 CREATE TABLE `tags` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `tag` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` int(11) NOT NULL DEFAULT 0,
-  `group_id` int(11) NOT NULL DEFAULT 0
+  `group_id` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-ALTER TABLE `tags`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `tags`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 
 CREATE TABLE `credit_log` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `timestamp` bigint(20) NOT NULL,
   `type` varchar(255) NOT NULL DEFAULT 'stuff_store',
-  `credits` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `credits` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
-ALTER TABLE `credit_log`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `credit_log`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
