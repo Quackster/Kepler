4ad282e13b3888582ec0550e541d79b4b67da23a
Quackster <quacksterr.15@gmail.com>
2022-09-14 21:21:03 +1000

    Update Github Actions workflow to not destroy Bouncycastle jar signature

---
+++ tools/scripts/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+CLASSPATH=kepler.jar:lib/HikariCP-3.4.1.jar:lib/mariadb-java-client-2.3.0.jar:lib/netty-all-4.1.33.Final.jar:lib/slf4j-log4j12-1.7.25.jar:lib/slf4j-api-1.7.25.jar:lib/log4j-1.2.17.jar:lib/commons-configuration2-2.2.jar:lib/commons-lang3-3.9.jar:lib/commons-lang-2.6.jar:lib/commons-validator-1.6.jar:lib/gson-2.8.0.jar:lib/spring-security-crypto-5.7.3.jar:lib/bcprov-jdk15on-1.70.jar:lib/chesslib-1.1.1.jar:lib/commons-beanutils-1.9.2.jar:lib/commons-logging-1.2.jar:lib/commons-digester-1.8.1.jar:lib/commons-collections-3.2.2.jar
+
+java -classpath $CLASSPATH org.alexdev.kepler.Kepler
