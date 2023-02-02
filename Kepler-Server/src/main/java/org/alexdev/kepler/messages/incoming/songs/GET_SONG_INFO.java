05658d4683e75c36c2ba51a3db844d9f9eb4bd3b
Patrick Nærvig Wøhlk <patrick@webbanditten.dk>
2023-02-02 17:13:22 +0100

    fix: jukebox

--- Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/songs/GET_SONG_INFO.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/songs/GET_SONG_INFO.java
@@ -21,7 +21,7 @@ public class GET_SONG_INFO implements MessageEvent {
         if (room.getItemManager().getSoundMachine() == null) {
             return;
         }
-
+/*
         player.send(new USER_SOUND_PACKAGES(player.getInventory().getSoundsets()));
         player.send(new SOUND_PACKAGES(SongMachineDao.getTracks(room.getItemManager().getSoundMachine().getId())));
 
@@ -29,28 +29,22 @@ public class GET_SONG_INFO implements MessageEvent {
 
         if (song.size() > 0) {
             player.send(new SONG_INFO(song.get(0)));
-        }
+        }*/
 
-        /*
+
         int songId = -1;
 
-        if (player.getVersion() <= 14) {
-            songId = room.getItemManager().getSoundMachine().getId();
 
-            player.send(new USER_SOUND_PACKAGES(player.getInventory().getSoundsets()));
-            player.send(new SOUND_PACKAGES(SongMachineDao.getTracks(room.getItemManager().getSoundMachine().getId())));
-        } else {
-            try {
-                songId = reader.readInt();
-            } catch (Exception ex){
-                throw new Exception("Replace your fucking CCTs with the ones in /tools/v15_ccts.zip if you're running this on v15 port");
-            }
+        try {
+            songId = reader.readInt();
+        } catch (Exception ex){
+            throw new Exception("Replace your fucking CCTs with the ones in /tools/v15_ccts.zip if you're running this on v15 port");
         }
 
         var song = SongMachineDao.getSong(songId);
 
         if (song != null) {
             player.send(new SONG_INFO(SongMachineDao.getSong(songId)));
-        }*/
+        }
     }
 }
