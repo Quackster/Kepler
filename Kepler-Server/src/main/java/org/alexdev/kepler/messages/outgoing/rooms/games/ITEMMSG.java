666f793a3fa25b86c311bd9762ac10a69b772875
wackfx <contact@wackfx.com>
2024-06-24 04:56:19 +0200

    feat: Poker v14

--- Kepler-Server/src/main/java/org/alexdev/kepler/messages/outgoing/rooms/games/ITEMMSG.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/messages/outgoing/rooms/games/ITEMMSG.java
@@ -5,15 +5,35 @@ import org.alexdev.kepler.server.netty.streams.NettyResponse;
 
 public class ITEMMSG extends MessageComposer {
     private final String[] commands;
+    private final boolean delimitate;
 
+    /**
+     * Create a new ITEMMSG with multiple commands.
+     * Each line will be delimited with '\r'
+     * @param commands
+     */
     public ITEMMSG(String[] commands) {
         this.commands = commands;
+        this.delimitate = true;
+    }
+
+    /**
+     * Create a new ITEMMSG with a single command.
+     * This expects the command to be already delimited
+     * @param command
+     */
+    public ITEMMSG(String command) {
+        this.commands = new String[]{command};
+        this.delimitate = false;
     }
 
     @Override
     public void compose(NettyResponse response) {
         for (String value : this.commands) {
-            response.writeDelimeter(value, '\r');
+            if (this.delimitate)
+                response.writeDelimeter(value, '\r');
+            else
+                response.write(value);
         }
     }
 
