17a59cd2a0dbdfd3654d8d5110b273ef83abbc50
Patrick Nærvig Wøhlk <patrick@webbanditten.dk>
2024-07-23 20:23:43 +0200

    fix: room update from website

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/commandqueue/commands/RoomUpdateCommand.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/commandqueue/commands/RoomUpdateCommand.java
@@ -2,6 +2,7 @@ package org.alexdev.kepler.game.commandqueue.commands;
 
 import org.alexdev.kepler.dao.mysql.RoomDao;
 import org.alexdev.kepler.game.commandqueue.CommandTemplate;
+import org.alexdev.kepler.game.navigator.NavigatorManager;
 import org.alexdev.kepler.game.player.Player;
 import org.alexdev.kepler.game.player.PlayerManager;
 import org.alexdev.kepler.game.room.Room;
@@ -12,12 +13,76 @@ public class RoomUpdateCommand implements Command {
     public void executeCommand(CommandTemplate commandArgs) {
         var room = RoomDao.getRoomById(commandArgs.RoomId);
         if(room != null) {
+
+            System.out.println(room.getData().getId());
+            System.out.println(room.getData().getVisitorsMax());
+            System.out.println(room.getData().getAccessType());
+
+
+            /*
+            *
+        String contents = reader.contents();
+
+        if (contents.startsWith("/")) {
+            contents = contents.substring(1);
+        }
+
+        int roomId = Integer.parseInt(contents.split("/")[0]);
+
+        Room room = RoomManager.getInstance().getRoomById(roomId);
+
+        if (room == null) {
+            return;
+        }
+
+        if (!room.isOwner(player.getDetails().getId())) {
+            return;
+        }
+
+        String[] data = contents.split(Character.toString((char) 13));
+
+        for (String setting : data) {
+            int index = setting.indexOf("=");
+
+            if (index == -1) {
+                continue;
+            }
+
+            String key = setting.substring(0, index);
+            String value = setting.substring(index + 1);
+
+            if (key.startsWith("description")) {
+                room.getData().setDescription(StringUtil.filterInput(value, true));
+            }
+
+            if (key.startsWith("allsuperuser")) {
+                room.getData().setSuperUsers(Integer.parseInt(value) == 1);
+            }
+
+            if (key.startsWith("maxvisitors")) {
+                int maxVisitors = Integer.parseInt(value);
+
+                if (maxVisitors < 10 || maxVisitors > 50) {
+                    maxVisitors = 25;
+                }
+
+                room.getData().setVisitorsMax(maxVisitors);
+            }
+
+            if (key.startsWith("password")) {
+                room.getData().setPassword(StringUtil.filterInput(value, true));
+            }
+        }
+
+        RoomDao.save(room);*/
             room.getData().setName(commandArgs.RoomName);
             room.getData().setDescription(commandArgs.RoomDescription);
             room.getData().setShowOwnerName(commandArgs.RoomShowOwnerName);
             room.getData().setAccessType(commandArgs.RoomAccessType);
 
             RoomDao.save(room);
+
+            NavigatorManager.getInstance().resetCategoryMap();
         }
     }
 }
--- Kepler-Server/src/main/java/org/alexdev/kepler/messages/outgoing/rooms/settings/FLATINFO.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/messages/outgoing/rooms/settings/FLATINFO.java
@@ -18,24 +18,43 @@ public class FLATINFO extends MessageComposer {
 
     @Override
     public void compose(NettyResponse response) {
-        response.writeBool(this.room.getData().allowSuperUsers());
-        response.writeInt(this.room.getData().getAccessTypeId());
-        response.writeInt(this.room.getId());
+        response.writeBool(this.room.getData().allowSuperUsers()); // #ableothersmovefurniture
+        response.writeInt(this.room.getData().getAccessTypeId()); // #door
+        response.writeInt(this.room.getId()); // #flatId
 
         if (this.room.isOwner(player.getDetails().getId())|| this.room.getData().showOwnerName() || this.player.hasFuse(Fuse.SEE_ALL_ROOMOWNERS)) {
-            response.writeString(this.room.getData().getOwnerName());
+            response.writeString(this.room.getData().getOwnerName()); // #owner
         } else {
-            response.writeString("-");
+            response.writeString("-"); // #owner
         }
 
-        response.writeString(this.room.getModel().getName()); // Is called "marker" in Lingo code
-        response.writeString(this.room.getData().getName());
-        response.writeString(this.room.getData().getDescription());
-        response.writeBool(this.room.getData().showOwnerName());
-        response.writeBool(this.room.getCategory().hasAllowTrading()); // Allow trading
-        response.writeBool(this.room.getCategory() == null);
-        response.writeInt(this.room.getData().getVisitorsNow());
-        response.writeInt(this.room.getData().getVisitorsMax());
+        response.writeString(this.room.getModel().getName()); // #marker
+        response.writeString(this.room.getData().getName()); // #name
+        response.writeString(this.room.getData().getDescription()); // #description
+        response.writeBool(this.room.getData().showOwnerName()); // #showownername
+        response.writeBool(this.room.getCategory().hasAllowTrading()); // #trading
+        response.writeBool(false); // #alert
+        /* Edited 23-07-2024 - WTF was Quackster thinking here?? Max visitors should be here not the current amount of visitors in the room.
+        * This fucked up the client, if you were joining a room, then editing it the "max visitors" would be represented as 1.
+        *
+        *       "nav_maxusers_minus":
+        * tMaxVisitors = integer(me.getComponent().getNodeProperty(tNodeId, #maxVisitors) - 5)
+        * if tMaxVisitors < 10 then
+        *    tMaxVisitors = 10
+        * end if
+        * getWindow(me.pWindowTitle).getElement("nav_maxusers_amount").setText(tMaxVisitors)
+        * me.getComponent().setNodeProperty(tNodeId, #maxVisitors, tMaxVisitors)
+        * "nav_maxusers_plus":
+        * tAbsoluteMax = me.getComponent().getNodeProperty(tNodeId, #absoluteMaxVisitors)
+        * tMaxVisitors = integer(me.getComponent().getNodeProperty(tNodeId, #maxVisitors) + 5)
+        * if tMaxVisitors > tAbsoluteMax then
+        *   tMaxVisitors = tAbsoluteMax
+         * end if
+        *
+        * TODO Add the absolute max visitors to the room model data and use that instead hardcoding 200
+        * */
+        response.writeInt(this.room.getData().getVisitorsMax()); // #maxVisitors
+        response.writeInt(200); // #absoluteMaxVisitors
     }
 
     @Override
